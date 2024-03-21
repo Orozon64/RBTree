efbc36c50d52b8b760f6441ebe851ae3468dc67b
Orozon64 <128033558+Orozon64@users.noreply.github.com>
2024-03-21 15:03:28 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,7 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-import java.util.ArrayList;
-import java.util.Random;
+public class Main {
 
-class HelloWorld {
     public static void main(String[] args) {
-        RBTree new_tree = new RBTree();
-        new_tree.insert(0, 5, -1);
-        new_tree.insert(1, 8, 0);
-        new_tree.insert(2, 3, 0);
-        int get_value = new_tree.get(2);
-        System.out.println(get_value);
-        int height_val = new_tree.height();
-        System.out.println(height_val);
-        int remove_val = new_tree.remove(1);
-        System.out.println(remove_val);
+        System.out.println("Hello World!");
     }
 }
 class Node{
@@ -27,13 +14,13 @@ class Node{
 
     Color color;
 
-    
-    
-    public Node(Color c, int k, int v{
+
+
+    public Node(Color c, int k, int v){
         color = c;
         key = k;
         value = v;
-        
+
     }
 
 }
@@ -47,33 +34,46 @@ class RBTree{
             root = new Node(Node.Color.BLACK, key, value);
         }
         else{
-            
-            newnode = new Node(Node.Color.RED, key, value); //przy wstawianiu zawsze ustawiamy kolor na czerwony, potem poprawimy
+
+            Node newnode = new Node(Node.Color.RED, key, value); //przy wstawianiu zawsze ustawiamy kolor na czerwony, potem poprawimy
             lastnode = root;
-        while(!inserted){
-            if(lastnode.value < value && lastnode.rightchild == null){
-                lastnode.rightchild = newnode;
+            while(!inserted){
+                if(lastnode.value < value && lastnode.rightchild == null){
+                    lastnode.rightchild = newnode;
+                    inserted = true;
+                }
+                else if(lastnode.value < value && lastnode.rightchild != null){
+                    lastnode = lastnode.rightchild;
+                }
+                else if(lastnode.value > value && lastnode.leftchild == null){
+                    lastnode.leftchild = newnode;
+                    inserted = true;
+                }
+                else if(lastnode.value > value && lastnode.leftchild != null){
+                    lastnode = lastnode.leftchild;
+                }
+                else if(lastnode.value == value){
+                    if(lastnode.leftchild == null){ //wartości równe będziemy wstawiać po lewej
+                        lastnode.leftchild = newnode;
+                        inserted = true;
+                    }
+                    else{
+                        lastnode = lastnode.leftchild;
+                    }
+                }
             }
-            else if(lastnode.value < value && lastnode.rightchild != null){
-                lastnode = lastnode.rightchild;
-            }
-            else if(lastnode.value > value && lastnode.leftchild == null){
-                lastnode.leftchild = newnode;
-            }
-            else if(lastnode.value > value && lastnode.leftchild != null){
-                lastnode = lastnode.leftchild;
-            }
-        }    
-            
+
         }
 
     }
-   int get(int key){
+    int get(int key){
         Node currentNode = root;
-        while (currentNode != null){
+        int found_value = 0;
+        boolean found = false;
+        while (!found){
             if(currentNode.key == key){
-                return curNode.value;
-                break;
+                found_value =  currentNode.value;
+                found = true;
             }
             else if(key < currentNode.key){
                 if(currentNode.leftchild != null){
@@ -81,7 +81,7 @@ class RBTree{
                 }
                 else{
                     return -1;
-                    break;
+
                 }
             }
             else{
@@ -93,44 +93,33 @@ class RBTree{
                 }
             }
         }
+        return found_value;
     }
     int remove(int key){
-        boolean is_found = false;
-        int found_position = -1;
-        for(int i = 0; i < nodes_in_tree.size(); i++){
-            Node current_node = nodes_in_tree.get(i);
-            if(key == current_node.key){
-                is_found = true;
-                found_position = i;
-            }
-
-        }
-        if(is_found){
-            int value_to_return;
-            Node node_to_remove;
-            node_to_remove = nodes_in_tree.get(found_position);
-            value_to_return = node_to_remove.value;
-            nodes_in_tree.remove(found_position);
-            for(int i = 0; i < nodes_in_tree.size(); i++){
-                Node current_node = nodes_in_tree.get(i);
-                if(current_node.key_of_parent == key){
-                    nodes_in_tree.remove(i);
-                }
-            }
-            return value_to_return;
-
-        }
-        else{
-
-            return -1;
-        }
+        Node currentNode = root;
+        if()
     }
 
     int height(){
-        int last_layer = 0;
-        for (Node loopnode : nodes_in_tree) {
-            if (loopnode.layer_in_tree > last_layer) {
-                last_layer = loopnode.layer_in_tree;
+        int last_layer = 1;
+        Node currentNode = root;
+        boolean has_reached_bottom = false;
+        while (!has_reached_bottom){
+            if(currentNode.leftchild == null && currentNode.rightchild == null){
+
+                has_reached_bottom = true;
+
+            }
+            else if(currentNode.leftchild != null && currentNode.rightchild == null){
+                last_layer++;
+                currentNode = currentNode.leftchild;
+            }
+            else if(currentNode.leftchild == null && currentNode.rightchild != null){
+                last_layer++;
+                currentNode = currentNode.rightchild;
+            }
+            else{
+                //jak uniknąć fałszwych wyników?
             }
         }
         return last_layer;
