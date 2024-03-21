ea389fe9458ba69d83fde3468833b014df9a9bd3
Orozon64 <128033558+Orozon64@users.noreply.github.com>
2024-03-21 08:58:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+package com.company;
+
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
+
 class Node{
     String color;
     int key;
@@ -22,7 +24,7 @@ class RBTree{
 
 
     Random rand = new Random();
-    ArrayList<Node> nodes_in_tree = new ArrayList<Node>();
+    ArrayList<Node> nodes_in_tree = new ArrayList <Node>();
     void insert(int key, int value, int parent_key){
         String insert_col = "";
         int xpos = 0;
@@ -31,28 +33,25 @@ class RBTree{
             insert_col = "black";
         }
         else{
-            for(int i = 0; i < nodes_in_tree.size(); i++){
-                Node loopNode = nodes_in_tree.get(i);
-                if(loopNode.key == parent_key){
-                    if(loopNode.color.equals("red")){
+            for (Node loopNode : nodes_in_tree) {
+                if (loopNode.key == parent_key) {
+                    if (loopNode.color.equals("red")) {
                         insert_col = "black";
-                    }
-                    else{
+                    } else {
                         int color_decider = rand.nextInt(2); //jeśli rodzicem jest obiekt czarny, dziecko może być czerwone lub czarne, więc możemy to wylosować.
-                        switch(color_decider){
+                        switch (color_decider) {
                             case 0:
                                 insert_col = "red";
                                 break;
                             default:
                                 insert_col = "black";
-
+                                break;
                         }
                     }
                     layer_of_new_node = loopNode.layer_in_tree + 1;
-                    if(value < loopNode.value){
-                        xpos = loopNode.x_position -1;
-                    }
-                    else {
+                    if (value < loopNode.value) {
+                        xpos = loopNode.x_position - 1;
+                    } else {
                         xpos = loopNode.x_position + 1;
                     }
                 }
@@ -61,15 +60,15 @@ class RBTree{
         }
         Node new_node = new Node(insert_col, key, value, layer_of_new_node, parent_key, xpos);
         nodes_in_tree.add(new_node);
+        System.out.println("Wstawiono nowy węzeł o kolorze " + insert_col + " kluczu: " + key + " wartości " + value + " warstwie " + " kluczu rodzica " + " i pozycji lewo-prawo " + xpos);
     }
     int get(int key){
 
 
         int found_value = -1;
 
-        for(int i = 0; i < nodes_in_tree.size(); i++){
-            Node current_node = nodes_in_tree.get(i);
-            if(key == current_node.key){
+        for (Node current_node : nodes_in_tree) {
+            if (key == current_node.key) {
 
                 found_value = current_node.value;
 
@@ -89,14 +88,18 @@ class RBTree{
 
         }
         if(is_found){
+            int value_to_return;
+            Node node_to_remove;
+            node_to_remove = nodes_in_tree.get(found_position);
+            value_to_return = node_to_remove.value;
             nodes_in_tree.remove(found_position);
             for(int i = 0; i < nodes_in_tree.size(); i++){
                 Node current_node = nodes_in_tree.get(i);
                 if(current_node.key_of_parent == key){
-                    remove(current_node.key);
+                    nodes_in_tree.remove(i);
                 }
             }//jeśli usuwamy rodzica, trzeba usunąć też jego dzieci.
-            return found_position;
+            return value_to_return;
 
         }
         else{
@@ -107,9 +110,8 @@ class RBTree{
 
     int height(){
         int last_layer = 0;
-        for(int i = 0; i < nodes_in_tree.size(); i++){
-            Node loopnode = nodes_in_tree.get(i);
-            if(loopnode.layer_in_tree > last_layer){
+        for (Node loopnode : nodes_in_tree) {
+            if (loopnode.layer_in_tree > last_layer) {
                 last_layer = loopnode.layer_in_tree;
             }
         }
@@ -117,8 +119,8 @@ class RBTree{
     }
 
 }
+public class Main {
 
-class HelloWorld {
     public static void main(String[] args) {
         RBTree new_tree = new RBTree();
         new_tree.insert(0, 5, -1);
